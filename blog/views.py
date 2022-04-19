f56342dc9de46b7e1ad570840621a16589e308c2
jyn-choi <choijieun@hanwha.com>
2022-04-19 08:40:47 +0000

    edit

--- blog/views.py
+++ blog/views.py
@@ -26,8 +26,8 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        # form = PostForm()
-        form = PostForm(request.POST, instance=post)
+        form = PostForm()
+        # form = PostForm(request.POST, instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
     
     
